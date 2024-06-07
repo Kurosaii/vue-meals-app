616373e7ff7f4d7c1f7760bf0c418caa20fa53cb
Benjamin Evan Cowles <becowles93@gmail.com>
2024-06-06 22:07:19 -0400

    feat: Add search and letters links to Home

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,18 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import HomeView from "../views/HomeView.vue";
+import MealListView from "../views/MealListView.vue";
 
 const routes = [
   {
     path: "/",
     name: "home",
     component: HomeView,
+  },
+  {
+    path: "/letter/:letter",
+    name: "byLetter",
+    component: MealListView,
   },
 ];
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,9 +2,17 @@
 import store from '../store';
 import { computed } from 'vue';
 
-const meals = computed(() => store.state.meals);
+const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
 </script>
 
 <template>
-    <pre>{{ meals }}</pre>
+    <div class="flex flex-col justify-center p-8">
+        <input type="text" class="border-gray-200 border-2 rounded w-full" placeholder="Search for meals" />
+
+        <div class="flex gap-2 mt-2">
+            <router-link v-for="letter in letters" :key="letter" :to="{ name: 'byLetter', params: { letter } }">
+                {{ letter }}
+            </router-link>
+        </div>
+    </div>
 </template>
